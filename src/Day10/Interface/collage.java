6e28cfe106a4b0ff64b5119f6a2db0a9e6066809
Durgesh Dandage <dandagedurgesh@gmail.com>
2024-06-12 13:09:41 +0530

    Initial commit

---
+++ src/Day10/Interface/collage.java
@@ -0,0 +1,7 @@
+package Day10.Interface;
+
+public interface collage {
+    void display();
+    int showRollNo();
+}
+
---
+++ src/Day10/Interface_Advance/Trial.java
@@ -0,0 +1,7 @@
+package Day10.Interface_Advance;
+
+public interface Trial {
+
+    void display();
+    int showRollNo();
+}
